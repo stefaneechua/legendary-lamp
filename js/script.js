685010fb40166d2297aefc40e1b7af8c5af686f2
stefaneechua <stefaneechua@gmail.com>
2017-11-28 12:07:11 +1300

    covered until functions from w3schools

--- js/script.js
+++ js/script.js
@@ -3,10 +3,22 @@ function changeText() {
   document.getElementById("xmas").innerHTML = "Idk";
 }
 
+// changes the image depending on button pressed
+var elem = document.getElementById("santa");
+
+
+function toggleSanta() {
+  document.getElementById("santa").src="http://piq.codeus.net/static/media/userpics/piq_155228_400x400.png";
+  elem.style.backgroundColor = "green"; //adding css styles
+}
+
 function togglePizza() {
   document.getElementById("santa").src="https://www.spreadshirt.com/image-server/v1/mp/designs/1000500549,width=178,height=178/8bit-pizza.png";
 }
 
-function toggleSanta() {
-  document.getElementById("toggleSanta").src="http://piq.codeus.net/static/media/userpics/piq_155228_400x400.png";
-}
+// output
+// window.alert("hello xmas");
+
+// Booleans
+var x = 10; y = "10"; z = "sandy claws";
+  document.getElementById("demo").innerHTML = (x == y) + "<br>" + (y === z) + "<br>" + (x === y);
