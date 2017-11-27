b5a059e0d597f83bece2d1f5fb405bd3cc38c1a7
stefaneechua <stefaneechua@gmail.com>
2017-11-28 09:11:33 +1300

    working on lightbulb exercise

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// changes the text once button is clicked
+function changeText() {
+  document.getElementById("xmas").innerHTML = "Idk";
+}
+
+function togglePizza() {
+  document.getElementById("santa").src="https://www.spreadshirt.com/image-server/v1/mp/designs/1000500549,width=178,height=178/8bit-pizza.png";
+}
+
+function toggleSanta() {
+  document.getElementById("toggleSanta").src="http://piq.codeus.net/static/media/userpics/piq_155228_400x400.png";
+}
